d4922c91e1b11a994a584359a16da63fdeeebe3c
davue <davue@bluewin.ch>
2020-04-04 12:41:27 +0200

    Updated the protection ticket

--- src/main/java/ch/ethz/geco/gecko/ticket/impl/ProtectionTicket.java
+++ src/main/java/ch/ethz/geco/gecko/ticket/impl/ProtectionTicket.java
@@ -6,8 +6,9 @@ public class ProtectionTicket extends BaseTicket {
     public ProtectionTicket(Snowflake issuer) {
         super(issuer);
 
-        this.questions.add("Please provide the X/Z coordinates of your building / property:");
-        this.questions.add("List all Minecraft usernames of players, which should be allowed to build:");
+        this.questions.add("Please provide the rough X/Z coordinates of your building / property:");
+        this.questions.add("Please mark the corners of the area with some blocks and state the block type here:");
+        this.questions.add("List all Minecraft usernames of players, which should be allowed to build (separated by commas or spaces):");
         this.questions.add("Should PvP be allowed?");
         this.questions.add("Any comments?");
 
